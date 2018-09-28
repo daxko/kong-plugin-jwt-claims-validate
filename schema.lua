118122daf794f7cf8318bcead6a95ff1c5284b22
Wally S <wshirey@daxko.com>
2018-09-28 17:05:07 -0500

    Update schema.lua

--- schema.lua
+++ schema.lua
@@ -1,26 +1,33 @@
 local cjson = require "cjson"
 
-local function claim_check(value, conf)
-  local valid_types = {
+local valid_types = {
     ["string"]  = true, 
     ["boolean"] = true,
     ["number"]  = true
-  }
+}
 
+local function claim_check(value, conf)
   for k,v in pairs(value) do
     local type = type(v)
-    if not valid_types[type] or type == cjson.null then
+    if not is_scalar(v) then
       return false, "'claims."..k.."' is not one following types: [boolean, string, number]"
     end
-
-    return true, nil
   end
+  return true, nil
 end
 
 return {
   no_consumer = true,
   fields = {
-    uri_param_names = {type = "array", default = {"jwt"}},
-    claims = { type = "table", func = claim_check }
+    uri_param_names = { type = "array", default = { "jwt" } },
+    claims = { 
+      type = "table", 
+      func = claim_check,
+      new_type = {
+        type = "map",
+        keys = { type = "string" },
+        values = { type = "skip", custom_validator = is_scalar },
+      }
+    }
   }
 }
